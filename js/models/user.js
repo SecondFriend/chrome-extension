58244ef07f92f6bfe94cad58fc3ec28cb0725767
Jan Järfalk <jan@jarfalk.se>
2012-06-21 08:42:52 +0200

    Injected local storage into backbone
    Quickfix FTW

--- js/models/user.js
+++ js/models/user.js
@@ -10,14 +10,12 @@ define([
   return Backbone.Model.extend({
 
     localStorage: new Store("User"),
-    defaults: {
       'nickname': '',
       'firstname': '',
       'lastname': '',
       'email': '',
       'phone': '',
       'address': '',
-    },
 
     initialize: function () {
       this.fetch();
