783841b1c7734b3dc585abbb2e2ee37dd495256f
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-06-21 10:50:53 +0100

    Properly format timestamp

--- js/views/message.js
+++ js/views/message.js
@@ -1,41 +1,41 @@
 define([
 
-	'underscore',
-	'backbone',
-	'jquery'
-	
-], function( _, Backbone, jQuery ){
-	
-	var view = Backbone.View.extend({
-		'tagName'	: 'li',
-		'className'	: 'ui-li ui-body-c ui-li-static',
-		'events'	: {},
-		'template': _.template($('#tpl-chat-message').html()),
-			
-		'initialize': function(){
+  'underscore',
+  'backbone',
+  'jquery'
 
-    
-			_.bindAll(this, 'render');
-			//this.model.view = this;
-		
-		},
-	
-		
-		'render' : function(){	
-    		
-    		console.log( this.model.toJSON() );
-    		
-    		if(!this.model.toJSON().message) return false;
-    		
-			$( this.el ).html( this.template(this.model.toJSON()) ).addClass( this.model.get('type') );
-		
-			return this.el;
-		
-		}
-		
-		
-	});
-	
-	return view;
+], function( _, Backbone, jQuery ){
+
+  _.mixin({
+    'fmtTime' : function(timetoken) {
+      var now = new Date(timetoken/10000);
+      return now.getHours()+':'+now.getMinutes()+':'+now.getSeconds();
+    },
+  });
+
+  var view = Backbone.View.extend({
+    'tagName' : 'li',
+    'className' : 'ui-li ui-body-c ui-li-static',
+    'events'  : {},
+    'template': _.template($('#tpl-chat-message').html()),
+
+    'initialize': function(){
+
+      _.bindAll(this, 'render');
+
+    },
+
+    'render' : function(){
+
+      if(!this.model.toJSON().message) return false;
+
+      $( this.el ).html( this.template(this.model.toJSON()) ).addClass( this.model.get('type') );
+
+      return this.el;
+    }
+
+  });
+
+  return view;
 
 });
