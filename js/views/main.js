6532422fe2332ec57e591e42be1eb4cb82da734d
Jan Järfalk <jan@jarfalk.se>
2012-06-21 12:13:23 +0200

    New hero on login

--- js/views/main.js
+++ js/views/main.js
@@ -54,12 +54,7 @@ define([
     'render': function () {
       var root = this;
 
-      var data = {
-        'heading' : 'CHAT',
-        'loadingMessage' : 'You are being connected to a secure and anonymous chat. Please wait a moment...',
-        'grownupPhoto' : '/fixtures/images/grownup.png',
-        'grownupName' : 'Camille Waddington'
-      };
+      var data = {};
 
       var html = _.template( this.template, data );
 
