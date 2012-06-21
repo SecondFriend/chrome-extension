766c4b2aadf8e8f49b13c6fd62a3277ca5329183
Florian Rathgeber <florian.rathgeber@gmail.com>
2012-06-21 12:26:22 +0100

    Send nickchange after councelor was assigned

--- js/views/chatclient.js
+++ js/views/chatclient.js
@@ -80,12 +80,6 @@ define([
                               .done( function() { console.log("Notification of backend successful"); })
                               .fail( function() { console.error("Notification of backend failed"); });
 
-                             // Notify the backend about my nickname
-                             root.messageSend('system', {
-                               action: 'changenick',
-                               nickname: App.get("nickname")
-                             })
-
                              // Initiate a chat with a consultant then do this.
                              // Unset loading sate
                              // Set loaded state
@@ -179,6 +173,12 @@ define([
             case 'counselor':
               App.Counselor = message.payload;
               App.trigger('new-counselor');
+
+              // Notify the backend about my nickname
+              this.messageSend('system', {
+                action: 'changenick',
+                nickname: App.get("nickname")
+              });
               break;
           }
 
