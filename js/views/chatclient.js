bbbef4d093c21e94ce3baebdfaa5cd31f7827dbf
soe <soe@soe.im>
2013-02-05 03:00:41 +0100

    minor fix

--- js/views/chatclient.js
+++ js/views/chatclient.js
@@ -47,6 +47,7 @@ define([
 
           // Generate uuid if we don't yet have one
           root.pubnub.uuid( function(uuid) {
+            console.log('uuid: '+ uuid);
             if (!App.has("uuid")) {
               console.log("Generated uuid: "+uuid);
               App.set({"uuid": uuid});
